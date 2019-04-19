a635e7e703aeae1f761da28e0cf45e582487eb55
Shrutimoy Das <shrutimoy.das@abacus.iiit.ac.in>
2019-04-19 19:06:03 +0530

    file read

--- code/main.c
+++ code/main.c
@@ -14,19 +14,21 @@ Here,
 	Hss is the reduced structure matrix.
 In the code below, CAM_PARAMS refer to the EO and IO camera parameters and
 STRUCT_PARAMS refer to the no of structure parameters which is 3 x no of object points.
-The matrices are read from files with the name of the blocks(Hcc,...).
-For ROMA dataset, atleast 5GB RAM is required
+The matrices are read from files with the name of the blocks(Hcc,...). In these files, the 
+first entry gives the no of non zeros in the matrix block.
+
+Compile as : cc -o main main.c st_io.c
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-#include<malloc.h>
 
 #include"read_coo_matrix.h"
+#include"st_io.h"
 
 #define CAM_PARAMS 353 		
 #define STRUCT_PARAMS 78963
-#define NNZ 3417034 
 
 //driver function
 int main()
@@ -35,8 +37,13 @@ int main()
 	double** Hcs;
 	double** Hsc;
 	double** Hss;
+	double** b;
 
-	Hcc = read_coo_matrix("Hcc",CAM_PARAMS,CAM_PARAMS,NNZ);
+	Hcc = read_coo_matrix("Hcc",CAM_PARAMS,CAM_PARAMS);
+	Hcs = read_coo_matrix("Hcs",CAM_PARAMS,CAM_PARAMS);
+	Hsc = read_coo_matrix("Hsc",CAM_PARAMS,CAM_PARAMS);
+	Hss = read_coo_matrix("Hss",CAM_PARAMS,CAM_PARAMS);
+	b = read_coo_matrix("b",CAM_PARAMS+STRUCT_PARAMS,1);
 
 	return 0;
 }
--- code/read_coo_matrix.h
+++ code/read_coo_matrix.h
@@ -1,10 +1,90 @@
 //this file contains code for reading the coo matrix from the respective files.
 //input param block type refers to the type of block to be read
 
-double** read_coo_matrix(char* block_type,int row,int col,int nnz)
+double** read_coo_matrix(char* block_type,int row,int col)
 {
 	double** H;
-	
+
+
+	if (strcmp(block_type,"Hcc") == 0)
+	{
+
+		H = read_file("/home/iiit/shrutimoy.das/Parallel-Domain-Decomposition/Results/Matrices/Hcs.txt");
+		
+	}
+
+
+
+	else if (strcmp(block_type,"Hcs") == 0)
+	{
+		if((fptr = fopen("/home/iiit/shrutimoy.das/Parallel-Domain-Decomposition/Results/Matrices/Hcs.txt","r")) == NULL)
+		{
+			printf("Error in opening Hcs.txt");
+			exit(1);
+		}
+		fscanf(fptr,"%d[^\n]",&nnz);
+		//printf("NNZ = %d\n", nnz); // 1589016
+		H = read_file(fptr,row,col,nnz);
+		fclose(fptr);
+	}
+	else if (strcmp(block_type,"Hsc") == 0)
+	{
+		if((fptr = fopen("/home/iiit/shrutimoy.das/Parallel-Domain-Decomposition/Results/Matrices/Hsc.txt","r")) == NULL)
+		{
+			printf("Error in opening Hsc.txt");
+			exit(1);
+		}
+		fscanf(fptr,"%d[^\n]",&nnz);
+		//printf("NNZ = %d\n", nnz); // 1589016
+		H = read_file(fptr,row,col,nnz);
+		fclose(fptr);
+	}
+	else if (strcmp(block_type,"Hss") == 0)
+	{
+		if((fptr = fopen("/home/iiit/shrutimoy.das/Parallel-Domain-Decomposition/Results/Matrices/Hss.txt","r")) == NULL)
+		{
+			printf("Error in opening Hss.txt");
+			exit(1);
+		}
+		fscanf(fptr,"%d[^\n]",&nnz);
+		//printf("NNZ = %d\n", nnz); // 236889
+		H = read_file(fptr,row,col,nnz);
+		fclose(fptr);
+	}
+	else if (strcmp(block_type,"b") == 0)
+	{
+		if((fptr = fopen("/home/iiit/shrutimoy.das/Parallel-Domain-Decomposition/Results/Matrices/b.txt","r")) == NULL)
+		{
+			printf("Error in opening b.txt");
+			exit(1);
+		}
+		fscanf(fptr,"%d[^\n]",&nnz);
+		//printf("NNZ = %d\n", nnz); // 79316
+		H = read_file(fptr,row,col,nnz);
+		fclose(fptr);
+	} 
 
 	return H;
+}
+
+//This function reads the COO representation from the file and stores the resp indices
+//in the arrays row_idx[],col_idx[],val[]
+double** read_file(char* input_filename)
+{
+	double** M;
+	int row_max;
+    int row_min;
+    int col_max;
+    int col_min;
+    int row;
+    int col;
+    int nnz;
+    int *row_idx;
+    int *col_idx;
+    double* val;
+
+	r8st_header_read(input_filename, &row_min, &row_max, &col_min, &col_max, &row, &col, &nst );
+	r8st_data_read (input_filename, row, col, nnz, row_idx, col_idx, val);
+
+	return M;
 }
