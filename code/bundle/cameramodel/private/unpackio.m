6cbddb8015a27dd51d87e1c0f1dfd06fc691e2f2
Shrutimoy Das <shrutimoy.das@abacus.iiit.ac.in>
2019-04-21 22:29:32 +0530

    code change

--- code/bundle/cameramodel/private/unpackio.m
+++
@@ -1,8 +0,0 @@
-function [pp,f,K,P,b]=unpackio(IO,nK,nP)
-%Unpack inner orientation vector.
-
-f=IO(1,:);
-pp=IO(2:3,:);
-b=IO(4:5,:);
-K=IO(5+(1:nK));
-P=IO(5+nK+(1:nP));
--- code/misc/vec.m
+++
@@ -1,7 +0,0 @@
-function v=vec(A)
-%VEC Vectorization operation.
-%
-%   V=VEC(A) applies the vectorization operation on A, i.e. returns
-%   the elements of A in column-major order.
-
-v=A(:);
