6c03acd60540c3d551886c60dd8cb62ec614c87b
Shrutimoy Das <shrutimoy.das@abacus.iiit.ac.in>
2019-04-24 14:06:31 +0530

    block ops

--- code/MPI/read_coo_matrix.h
+++ code/MPI/read_coo_matrix.h
@@ -13,41 +13,6 @@ typedef struct
 
 
 
-//this function converts the COO matrix into dense format
-double** densify(int row,int col,int* row_idx,int* col_idx,double* val,int nnz)
-{
-	double** M;
-	double* ptr;
-	int i,j,k;
-	unsigned int len;
-
-	len = sizeof(double*)*row + sizeof(double)*row*col;
-	//printf("\nlen -> %u\n",len);
-	M = (double**)malloc(len);
-
-	if(M == NULL)
-		printf("\nError in pointer allocation.\n");
-
-	ptr = M + row; // ptr pointing to the first element of the 2D array, since malloc 
-					//allocates contiguous memory.
-
-	for(i=0;i<row;i++)
-		M[i] = ptr + col*i; // each row pointer points to appropriate row now.
-
-	for(i=0;i<nnz;i++)
-	{
-		j = row_idx[i];
-		k = col_idx[i];
-		M[j][k] = val[i];
-	}
-
-	printf("\nM -> %lf\n", M[0][2]); // -1.4099e+07
-
-	return M;
-
-}
-
-
 //This function reads the COO representation from the file and stores the resp indices
 //in the arrays row_idx[],col_idx[],val[]. The function then returns a pointer to the structure containing the coo
 //representation.
